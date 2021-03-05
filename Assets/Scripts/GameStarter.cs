28591026ec23942d281c996d58f5be081761fa7d
Robert Poienar <poienar.robert.adam@gmail.com>
2021-03-05 17:09:11 +0200

    Fixed Play button

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameStarter : MonoBehaviour
 {
-    public static void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("PlayScene");
     }
